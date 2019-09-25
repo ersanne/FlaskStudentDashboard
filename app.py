dffc590a67003b57d383857e0bafdb81ff8525e0
ersanne <erik@eriksanne.com>
2019-09-25 18:58:18 +0100

    Added nav bar, login (WIP)

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('base.html'), 200
+    return render_template('layout.html'), 200
+
+
+@app.route("/login")
+def login():
+    return render_template('auth/login.html'), 200
+
+
+@app.route("/signup")
+def signup():
+    return render_template('auth/signup.html'), 200
 
 
 if __name__ == '__main__':
