5b8e037d787c16ad0db7a156d21089af028b4168
ersanne <K1j@3QePIVFz>
2019-10-06 14:32:57 +0100

    Added various pages (static ui only)

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
+
+
 @app.route('/')
 def hello_world():
     return render_template('layout.html'), 200
@@ -10,13 +15,30 @@ def hello_world():
 
 @app.route("/login")
 def login():
-    return render_template('auth/login.html'), 200
+    return render_template('login.html'), 200
 
 
 @app.route("/signup")
 def signup():
-    return render_template('auth/signup.html'), 200
+    return render_template('signup.html'), 200
+
+
+@app.route("/feed")
+def feed():
+    return render_template('feed.html'), 200
+
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html'), 200
+
+
+@app.route("/calender")
+def calender():
+    return render_template('calender.html'), 200
 
 
 if __name__ == '__main__':
-    app.run()
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
